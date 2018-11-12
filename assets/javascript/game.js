4b7a88e410076f7d7ca21081b50699eb6bb5a9df
ericmagallan <ericmagallan85@gmail.com>
2018-11-11 23:38:15 -0800

    Added pseudo code to js file.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,3 +4,25 @@ $(document).ready(function () {
 //Prompt player for letter.
 //Show the player status.
 //
+
+// While a word has not been guess {
+//     Show the player their current progress
+//Get a guess from the player
+//}
+//If the player wants to quit the game {
+//Quit the game
+//}
+
+// //Else If {
+//     If the guess is not a single letter {
+//         Tell the player to pick a single letter
+//     }
+// }
+//Else {
+//     If the guess is in the word {
+//         Update the player's progress with the guess
+//     }
+// }
+// }
+
+//Congratulate the player on guessing the word.
