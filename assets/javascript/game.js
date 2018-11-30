a48139e0425f1d9c802aafb0975d45a42654a00e
ericmagallan <ericmagallan85@gmail.com>
2018-11-29 18:12:23 -0800

    Minor tweaks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,27 +2,27 @@
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 //Arrays and Variables for holding data. Another area that will house more variables and lists for the game.
-var wordOptions = [" bride", "groom", "vows", "bouquet", "veil", "wedding ring", "boutonniere", "tissues"];
-var selectedWord = "";
-var lettersInWord = [];
-var numBlanks = 0;
-var blanksAndSuccesses = [];
-var wrongLetters = [];
+    var wordBank = [" bride", "groom", "vows", "bouquet", "veil", "wedding ring", "boutonniere", "tissues"];
+    var chosenWord = "";
+    var lettersInWord = [];
+    var numBlanks = 0;
+    var blanksAndSuccesses = [];
+    var wrongLetters = [];
 
 //Game Counters
-var winCount = 0;
-var lossCount = 0;
-var guessesLeft = 6;
+    var winCount = 0;
+    var loserCount = 0;
+    var guessesLeft = 6;
 
 // Essential Functions (Reusable code that will be utilized multiiple times throughout the game
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 function startGame() {
-    selectedWord = wordOptions[Math.floor(Math.random() * wordOptions.length)];
-    lettersInWord = selectedWord.split("");
+    chosenWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+    lettersInWord = chosenWord.split("");
     numBlanks = lettersInWord.length;
 
     // Reset each round
-    guessesLeft = 9;
+    guessesLeft = 6;
     wrongLetters = [];
     blanksAndSuccesses = [];
 
@@ -32,8 +32,8 @@ function startGame() {
     }
 
 
-    // Testing/Debugging
-    console.log(selectedWord);
+    // Testing/Debugging Area
+    console.log(chosenWord);
     console.log(lettersInWord);
     console.log(numBlanks);
     console.log(blanksAndSuccesses);
